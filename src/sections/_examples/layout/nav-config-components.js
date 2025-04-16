caf3c0332f6c589a6a2447fac85af56f57af746c
Kevind Ospina <kevind171819@gmail.com>
2025-04-16 10:16:00 -0500

    first commit

---
+++ src/sections/_examples/layout/nav-config-components.js
@@ -0,0 +1,31 @@
+import { orderBy, kebabCase } from 'es-toolkit';
+
+import { CONFIG } from 'src/global-config';
+
+// ----------------------------------------------------------------------
+
+const componentNames = [
+  'Animate',
+  'Carousel',
+  'Utilities',
+  'Form validation',
+  'Form wizard',
+  'Icons',
+  'Image',
+  'Label',
+  'Lightbox',
+  'Markdown',
+  'Mega menu',
+  'Navigation bar',
+  'Scroll',
+  'Scroll progress',
+  'Player',
+];
+
+const createComponents = componentNames.map((name) => ({
+  name,
+  href: `/components/${kebabCase(name)}`,
+  icon: `${CONFIG.assetsDir}/assets/icons/components/ic-${kebabCase(name)}.svg`,
+}));
+
+export const allComponents = orderBy(createComponents, ['name'], ['asc']);
