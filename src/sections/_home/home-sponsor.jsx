833fe3af4fd797650583514f7c95a19ff89400f4
Profesional digital <profesionaldigital@impormedical.com.co>
2025-06-14 00:22:53 -0500

    Grid fixed

--- src/sections/_home/home-sponsor.jsx
+++ src/sections/_home/home-sponsor.jsx
@@ -9,56 +9,56 @@ const sponsors = [
     name: "Tinkko",
     altText: "Tinkko Logo",
     logoUrl: "https://imagedelivery.net/zbd8viznFTU9Xm-HIspwjQ/d8e02a22-53bb-4731-2ea0-3a7133cf3400/public",
-    imageHeight: { xs: "80px", sm: "80px", md: "120px" }
+    // imageHeight: { xs: "200px", sm: "80px", md: "120px" }
   },
   {
     id: 2,
     name: "España abogados",
     altText: "España abogados Logo",
     logoUrl: "https://imagedelivery.net/zbd8viznFTU9Xm-HIspwjQ/007d0578-c983-4ce9-b857-bead35e72b00/public",
-    imageHeight: { xs: "48px", sm: "60px", md: "80px" }
+    // imageHeight: { xs: "250px", sm: "60px", md: "80px" }
   },
   {
     id: 3,
     name: "Mazarik",
     altText: "Mazarik Logo",
     logoUrl: "https://imagedelivery.net/zbd8viznFTU9Xm-HIspwjQ/e6f075eb-ce27-43fc-6a96-0d5dfad0b100/public",
-    imageHeight: { xs: "48px", sm: "60px", md: "80px" }
+    // imageHeight: { xs: "48px", sm: "60px", md: "80px" }
   },
   {
     id: 4,
     name: "Inzolente",
     altText: "Inzolente Logo",
     logoUrl: "https://imagedelivery.net/zbd8viznFTU9Xm-HIspwjQ/4cd7a4a1-8e89-4a5b-e7b7-fbdcdff03b00/public",
-    imageHeight: { xs: "48px", sm: "60px", md: "80px" }
+    // imageHeight: { xs: "48px", sm: "60px", md: "80px" }
   },
   {
     id: 5,
     name: "Pronoia coffe company",
     altText: "Pronoia coffe company Logo",
     logoUrl: "https://imagedelivery.net/zbd8viznFTU9Xm-HIspwjQ/b1a4ebb9-1832-4698-dfcc-b4914bfb8800/public",
-    imageHeight: { xs: "48px", sm: "60px", md: "80px" }
+    // imageHeight: { xs: "48px", sm: "60px", md: "80px" }
   },
   {
     id: 6,
     name: "Avior Airlines",
     altText: "Avior Airlines Logo",
     logoUrl: "https://imagedelivery.net/zbd8viznFTU9Xm-HIspwjQ/fc0f975c-012d-4f03-5e29-e3068c30ca00/public",
-    imageHeight: { xs: "36px", sm: "44px", md: "46px" }
+    // imageHeight: { xs: "36px", sm: "44px", md: "46px" }
   },
   {
     id: 7,
     name: "Createka",
     altText: "Createka Logo",
     logoUrl: "https://imagedelivery.net/zbd8viznFTU9Xm-HIspwjQ/13f06e96-4b60-4db4-2020-300255478800/public",
-    imageHeight: { xs: "48px", sm: "60px", md: "80px" }
+    // imageHeight: { xs: "48px", sm: "60px", md: "80px" }
   },
   {
     id: 8,
     name: "Alerta Creative",
     altText: "Alerta Creative Logo",
     logoUrl: "https://imagedelivery.net/zbd8viznFTU9Xm-HIspwjQ/fffaa7c1-4998-4883-328c-4c412f09c500/public",
-    imageHeight: { xs: "36px", sm: "44px", md: "56px" }
+    // imageHeight: { xs: "36px", sm: "44px", md: "56px" }
   },
 ]
 
@@ -74,11 +74,16 @@ export function HomeSponsor() {
           {sectionTitle}
         </Typography>
       </Box>
-      <Grid
-        container
-        spacing={12}
-        justifyContent="center"
-        alignItems="center"
+      <Box
+        sx={(theme) => ({
+          display: 'grid',
+          gridTemplateColumns: 'repeat(2, 1fr)',
+          gap: 5,
+          justifyContent: 'center',
+          [theme.breakpoints.up('md')]: { 
+            gridTemplateColumns: 'repeat(4, 1fr)',
+          }
+        })}
       >
         {sponsors.map((sponsor) => (
           <Grid
@@ -90,14 +95,14 @@ export function HomeSponsor() {
             }}
           >
             {sponsor.logoUrl ? (
-              <Image
+              <img
                 src={sponsor.logoUrl}
                 alt={sponsor.altText}
-                sx={{
-                  height: sponsor.imageHeight || { xs: "48px", sm: "60px", md: "80px" },
+                style={{
+                  height: '100px',
+                  objectFit: 'contain',
                   width: "auto",
                   maxWidth: "100%",
-                  objectFit: "contain",
                 }}
                 loading="lazy"
               />
@@ -108,7 +113,7 @@ export function HomeSponsor() {
             )}
           </Grid>
         ))}
-      </Grid>
+      </Box>
     </Container>
   );
 }
